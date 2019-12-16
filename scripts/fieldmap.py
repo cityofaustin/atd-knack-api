602c310f3dacced9d964dfffb0ef8d227093ab1b
johnclary <john.clary@austintexas.gov>
2019-12-15 18:17:24 -0600

    moving stuff to root folder now

--- scripts/fieldmap.py
+++
@@ -1,9 +0,0 @@
-
-# this is just field data for data tracker
-transactions_data_tracker = {
-    'work_order_id' : 'field_1209_raw' # 'WRK18-011700''
-    'inventory_item_id' : 'field_1228_raw' # 49
-    'transaction_id' :  'field_770' # 338
-    'inventory_item_connection': 'field_513_raw' # [{'id': '5816474394b672442b51206a', 'identifier': 'Bull Dog Ped Button | EACH'}],
-    'knack_id' : 'id' # '5a831e138a73201d72a2fb16'
-}
