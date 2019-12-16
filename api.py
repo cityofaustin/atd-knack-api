a31c9e3d7679ca950f1579cd507c4f253493e56c
johnclary <john.clary@austintexas.gov>
2019-12-15 18:15:53 -0600

    handle work order payload and better logging

--- api.py
+++ api.py
@@ -4,24 +4,30 @@ Sanic server for integrating external functions and services.
 Add something like this to data tracker:
 
 $(document).on('knack-form-submit.view_2662', function(event, view, record) {
-    $.get("http://localhost:8000/inventory_request?src=5815f29f7f7252cc2ca91c4f&dest=123").done(function (data) {
+    var payload = JSON.stringify(record)
+    $.post("http://localhost:8000/inventory_request?src=5815f29f7f7252cc2ca91c4f&dest=5b422c9b13774837e54ed814", payload).done(function (data) {
         console.log(data);
     });
 });
 
+test:
+http://localhost:8000/inventory_request?src=5815f29f7f7252cc2ca91c4f&dest=5b422c9b13774837e54ed814&work_order=5df10bf86d76b100157d1003
+
 """
 from datetime import datetime
 import logging
+import pdb
 
 from sanic import Sanic
 from sanic import response
 from sanic import exceptions
 from sanic_cors import CORS, cross_origin
-import pdb
-from scripts import inventory_request as inv_req
+
+import _inventory_request
 
 app = Sanic()
 CORS(app)
+
 
 def _403(error):
     raise exceptions.Forbidden(error)
@@ -33,7 +39,7 @@ async def index(request):
     return response.text(f"ATD Knack API online at {now}")
 
 
-@app.route("/inventory_request")
+@app.route("/inventory_request", methods=["POST"])
 async def inventory_request(request):
     """
     Listen for an inventory request from a source (`src`) application and
@@ -41,13 +47,17 @@ async def inventory_request(request):
     """
     src = request.args.get("src")
     dest = request.args.get("dest")
+    work_order = request.json
 
     if not src or not dest:
         _403("`src` and `dest` are required.")
 
+    elif not work_order:
+        _403("`work_order` json is required.")
+
     try:
-        res = inv_req.main(src, dest)
-    
+        res = _inventory_request.main(src, dest, work_order)
+
     except Exception as e:
         # todo: debug only. this is not safe!
         # return a 5xx error instead.
@@ -59,5 +69,5 @@ async def inventory_request(request):
 if __name__ == "__main__":
     # todo: remove debug
     # todo: setup proper logging
-    logging.basicConfig(filename='error.log',level=logging.DEBUG)
+    logging.basicConfig(filename="error.log", level=logging.DEBUG)
     app.run(debug=True, host="0.0.0.0", port=8000)
