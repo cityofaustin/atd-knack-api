9a1095ef07166b281034227af2a92d51b5193081
johnclary <john.clary@austintexas.gov>
2019-12-29 11:48:44 -0600

    reduce to one route for all records

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ Add something like this to data tracker:
 
 $(document).on('knack-form-submit.view_2662', function(event, view, record) {
     var payload = JSON.stringify(record)
-    $.post("http://localhost:8000/inventory_request?src=5815f29f7f7252cc2ca91c4f&dest=5b422c9b13774837e54ed814", payload).done(function (data) {
+    $.post("http://localhost:8000/record?src=5815f29f7f7252cc2ca91c4f&dest=5b422c9b13774837e54ed814", payload, record_type="inventory_request").done(function (data) {
         console.log(data);
     });
 });
@@ -19,12 +19,11 @@ from sanic import response
 from sanic import exceptions
 from sanic_cors import CORS, cross_origin
 
-from secrets import KNACK_CREDENTIALS
-import _inventory_request
-import _inventory_txn
-import _create_account
-
+from _fieldmaps import fieldmap
 from _logging import get_logger
+import _record
+from secrets import KNACK_CREDENTIALS
+
 
 app = Sanic()
 CORS(app)
@@ -34,12 +33,26 @@ def _403(error):
     raise exceptions.Forbidden(error)
 
 
-def _validate_app_ids(app_ids):
+def _valid_app_ids(app_ids):
     """
     Ensure that the requested application IDs exist in the credential store.
     """
     for app_id in app_ids:
-        app_data = KNACK_CREDENTIALS[app_id]
+        try:
+            app_data = KNACK_CREDENTIALS[app_id]
+
+        except KeyError:
+            return False
+
+    return True
+
+
+def _valid_record_type(record_type):
+    try:
+        fieldmap = fieldmap[record_type]
+
+    except KeyError:
+        return False
 
     return True
 
@@ -50,102 +63,33 @@ async def index(request):
     return response.text(f"ATD Knack API online at {now}")
 
 
-@app.route("/inventory_request", methods=["POST"])
-async def inventory_request(request):
+@app.route("/record", methods=["POST"])
+async def record(request):
     """
-    TODO: udpate with separate docs for requests/txns
-    Facilitates inventory requests between the AMD Data Tracker and the Finance and
-    Purchasing system.
-
-    Listens for a work order from the Data Tracker and generates a corresponding
-    inventory request in the finance system.
-    --- 
-
-    When the request payload is a work order record dict from the Data Tracker,
-    it is handled like so:
-    
-    1. A new inventory request is created in the finance system, if one does not already
-    exist for this work order.
-    
-    2. The _inventory_requests script fetches any `READY_TO_SEND` item "transactions"
-    connected to this request.
-
-    3. The work order in the Data Tracker is updated with the Knack record ID of the
-    inventory request in the Finance system.
-    
-    ---
-
-    Record flows from the Finance system to the Data Tracker only occur when transactions
-    are updated in the Finance system, i.e., a transaction is issued or cancelled. All
-    inventory requests sent to the API from the Finance system will already have a 
-    corresponding work order in the Data Tracker, as well as connected transactions. The
-    work order in the Data Tracker is never updated by the Finance System, only
-    connected transactions are processed.
-
-    When the request payload is an inventory request from the Finance system:
-    
-    1. The API receives an inventory request, extracts the request ID, and fetches any
-    `READY_TO_SEND` item "transactions" connected to the request. These transactions are
-    fetched from a pre-filtered table view in the Finacne System.
-
-    2. Corresponding inventory transactions in the Data Tracker are updated accordingly.
-
-    3. The `transmission_status` of theaffected inventory transactions in the Finance sytem
-    are updated to confirm the request has been successfully sent to the data tracker.
-
-    """
-    src = request.args.get("src")
-    dest = request.args.get("dest")
-
-    data = request.json
-
-    if not src or not dest:
-        _403("`src` and `dest` are required.")
-
-    elif not data:
-        _403("`data` json is required.")
-
-    try:
-        # todo: test
-        _validate_app_ids([src, dest])
-
-    except:
-        _403("Unknown `src` or `dest` application ID(s) provided.")
-
-    try:
-        res = _inventory_request.main(src, dest, data)
-
-    except Exception as e:
-        # todo: debug only. this is not safe!
-        # return a 5xx error instead.
-        raise exceptions.ServerError(f"{e.__class__.__name__}: {e}")
-
-    return response.json(res)
-
-@app.route("/inventory_txn", methods=["POST"])
-async def inventory_txn(request):
-    """
-    Handle a transaction.
+    Handle a Knack record.
     """
     src = request.args.get("src")
     dest = request.args.get("dest")
     data = request.json
+    record_type = request.ags.get("type")
 
     if not src or not dest:
         _403("`src` and `dest` are required.")
 
-    elif not data:
+    if not data:
         _403("`data` json is required.")
 
-    try:
-        # todo: test
-        _validate_app_ids([src, dest])
+    if not record_tyoe:
+        _403("Record `type` is required.")
 
-    except:
+    if not _valid_app_ids([src, dest]):
         _403("Unknown `src` or `dest` application ID(s) provided.")
 
+    if not _valid_record_type(record_type):
+        _403("Unknown record `type` provided.")
+
     try:
-        res = _inventory_txn2.main(src, dest, data) #todo: create this
+        res = _record.main(src, dest, data, record_type=record_type)
 
     except Exception as e:
         # todo: debug only. this is not safe!
@@ -153,33 +97,6 @@ async def inventory_txn(request):
         raise exceptions.ServerError(f"{e.__class__.__name__}: {e}")
 
     return response.json({"todo": "todo"})
-
-
-@app.route("/create_account", methods=["POST"])
-async def create_account(request):
-    """
-    Create an account in the destination app which mirrors an account
-    in the source app.
-    """
-    src = request.args.get("src")
-    dest = request.args.get("dest")
-    data = request.json
-
-    if not src or not dest:
-        _403("`src` and `dest` are required.")
-
-    elif not data:
-        _403("`data` json is required.")
-
-    try:
-        res = _create_account.main(src, dest, data)
-
-    except Exception as e:
-        # todo: debug only. this is not safe!
-        # return a 5xx error instead.
-        raise exceptions.ServerError(f"{e.__class__.__name__}: {e}")
-
-    return response.json(res)
 
 
 if __name__ == "__main__":
