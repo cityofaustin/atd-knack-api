85f0d83873a5feb2ab9502a21242df8e8d0632d5
johnclary <john.clary@austintexas.gov>
2019-12-30 12:56:01 -0600

    happy docs

--- atd-knack-api/api.py
+++ atd-knack-api/api.py
@@ -1,28 +1,5 @@
 """
-Sanic server for integrating external functions and services.
-
-Add something like this to data tracker:
-
-$(document).on('knack-form-submit.view_2664', function(event, view, record) {
-    var src = Knack.application_id;
-    var dest = "5b422c9b13774837e54ed814"; // finance prod
-    var payload = JSON.stringify(record);
-    var type = "inventory_request";
-    $.post("http://localhost:8000/record?src=" + src + "&dest=" + dest + "&type=" + type, payload).done(function (data) {
-        console.log(data);
-
-        var token = Knack.getUserToken();
-        var headers = {"Authorization": token, 'X-Knack-Application-ID': src};
-        var url = "https://api.knack.com/v1/pages/scene_255/views/view_1966/records"
-        
-        $.ajaxSetup({headers:headers});
-
-        $.get(url, headers=headers).done(function(data) {
-            console.log(data)
-        })
-    });
-});
-
+Sanic server for integrating external functions and services to Knack.
 """
 from datetime import datetime
 import logging
@@ -79,7 +56,62 @@ async def index(request):
 @app.route("/record", methods=["POST"])
 async def record(request):
     """
-    Handle a Knack record.
+    Facilitates record transformation and copying between Knack applications.
+    Currently supports the exchange of inventory-related records between
+    Data Tracker and the Finance and Purchasing System.
+    
+    This service is designed for two-way communication between Knack applications,
+    and requires appropriate conifguration in the source and destination
+    applications. Notably, records in each application will need dedicated fields
+    to store the Knack record UUID of the cooresponding record in it's complement
+    application. This allows existing records to be updated, rather than created,
+    and it enables the setting of record connections across Knack objects. 
+
+    Request Parameters
+    ----------
+    src : str (required)
+        The application ID of the source application
+    
+    dest : str (required)
+        The application ID of the destination application
+
+    record_type : str (required)
+        The type of record to be transmitted. This value is used to retrieve the
+        correct fieldmap definitions which will be applied to the record translation.
+        Valide `record_type`s are defined in _fieldmaps.py.
+
+    data : Content-Type:application/json (required)
+        The Knack record data to be transmitted.
+    
+    Requests are handled like so:
+    
+    1. An API request is initated by custom CORS request that has been configured in
+    the source Knack application, typically after a form submission event.
+
+    2. The API validates that all required values are present, and validates that the
+    `src` and `dest` application IDs are known defined `secrets.py`, and that the
+    requested `record_type` is defined in `fieldmaps.py`.
+
+    3. Request data is translated to a new request payload according to its fieldmap.
+    
+    4. The translated source data is posted to the destination application.
+
+    5. The responsde data from the destination application is then translated to
+    the format of source application, and source application is updated accordingly.
+    
+    So, this endpoint triggers two record updates in response to
+    any request:
+        1) a record is created or updated in the destination application.
+        2) the originating record is updated in the src application.
+    
+    This behavior exists so that any time a record is created in a destination app
+    the original record is updated with additional data about it's state. E.g.,
+    updating a field which confirms that transmission was successful, or updating
+    the source record with Knack record ID in the destination application, which 
+    ensures that future record updates are processed as an update rather than a new record.
+
+    See `_fieldmaps.py` for further documentation on the mechanics of record translation,
+    notably the use of the `direction` parameter.
     """
     src = request.args.get("src")
     dest = request.args.get("dest")
