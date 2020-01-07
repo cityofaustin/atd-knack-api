8e0c8193e332460f5d8b62fd8fec009ba7618cfb
johnclary <john.clary@austintexas.gov>
2020-01-06 21:59:26 -0600

    add comment about setpath...there must be a better way...

--- atd_knack_api/api.py
+++ atd_knack_api/api.py
@@ -7,7 +7,7 @@ import logging
 from flask import Flask, request, abort
 from flask_cors import CORS
 
-# import _setpath
+# import _setpath # uncomment this for local development
 from atd_knack_api._fieldmaps import FIELDMAP
 from atd_knack_api._logging import get_logger
 from atd_knack_api import _inventory
