9608d10fc645e61681aa4f53d43f358ac67d1681
johnclary <john.clary@austintexas.gov>
2020-01-22 08:17:21 -0600

    wrap it in a func

--- scripts/set_env_vars.py
+++ scripts/set_env_vars.py
@@ -10,12 +10,17 @@ ENV = "dev"  # `dev` or `production`
 BUCKET = "atd-knack-api"  #
 FILENAME = "config.json"
 
-s3 = boto3.resource("s3")
-obj = s3.Object(BUCKET, FILENAME)
-config_raw = obj.get()["Body"].read()
-os.environ["knack_app_config"] = json.loads(config_raw).get(ENV).get("aws_environment_variables").get("knack_app_config")
+def load_credentials(env, bucket, filename):
+    s3 = boto3.resource("s3")
+    obj = s3.Object(bucket, filename)
+    config_raw = obj.get()["Body"].read()
+    os.environ["knack_app_config"] = json.loads(config_raw).get(env).get("aws_environment_variables").get("knack_app_config")
+    return True
 
-if os.getenv("knack_app_config"):
-    print("*** Credentials Loaded ***")
-else:
-    raise Exception("Failed to load credentials from S3")
+if not os.getenv("knack_app_config"):
+    load_credentials(ENV, BUCKET, FILENAME)
+
+    if os.getenv("knack_app_config"):
+        print("*** Credentials Loaded ***")
+    else:
+        raise Exception("Failed to load credentials from S3")
