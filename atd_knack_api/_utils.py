614d6bde4df7a68c79953aab1592998c9fb30eb3
johnclary <john.clary@austintexas.gov>
2020-01-24 13:44:04 -0600

    use auth in knackpy wrapper

--- atd_knack_api/_utils.py
+++ atd_knack_api/_utils.py
@@ -1,14 +1,21 @@
 import knackpy
 
-def knackpy_wrapper(cfg, app_id):
+def knackpy_wrapper(cfg, auth, filters=None):
     """
     Fetch records which need to be processed from a pre-filtered
     Knack view which does not require authentication.
     """
     return knackpy.Knack(
-        scene=cfg["scene"],
-        view=cfg["view"],
-        app_id=app_id,
+        scene=cfg.get("scene"),
+        view=cfg.get("view"),
+        obj=cfg.get("obj"),
+        app_id=auth["app_id"],
+        api_key=auth["api_key"],
         page_limit=100,
         rows_per_page=1000,
+        filters=filters,
     )
+
+
+def knack_filter(field, value):
+    return [{"field": f"{field}", "operator": "is", "value": f"{value}"}]
