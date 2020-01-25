b9d6c5a6f0ece61ce48af921f3b20b8b4204464d
johnclary <john.clary@austintexas.gov>
2020-01-25 10:28:11 -0600

    conditionally use connection in lookup

--- atd_knack_api/_transforms.py
+++ atd_knack_api/_transforms.py
@@ -16,14 +16,18 @@ would be stored as `WRK20-019384__&__am49c04mc4834al2
 LOOKUP_CACHE = {}
 CACHE_DELIMTTER = "__&__"
 
-def lookup_connection(val, config, auth):
+def lookup(val, config, auth):
     print(LOOKUP_CACHE)
     """
-    This transform is used for the setting of a connection field in the destination app.
+    This transform is used for fetching the knack record ID from the the destination
+    application, and returning the record ID as a str or list, as defined in the
+    `connection` key in the transform config. A list is returned for the purposes
+    of setting connection fields, a str is returned for the purpose of setting the
+    record ID of a record to be updated.
 
-    It queries the destination app for a record matching the input value (first apply any
-    "pre-transform" that was supplied). If a matching record is found, the record ID is
-    returned inside a list, as is required by the Knack API.
+    It queries the destination app for a record matching the input value (first applying
+    any "pre-transform" that was supplied). If a matching record is found, it is returned,
+    else None is returned
     """
     if config.get("pre_transform"):
         """
@@ -46,7 +50,11 @@ def lookup_connection(val, config, auth):
     if (kn.data_raw):
         # we take the first record that matches our filter. assume the `lookup_field_dest` val is unique
         # we transform it to a knack connection list
-        val_transformed = [kn.data_raw[0]["id"]]
+        if config.get("connection"):
+            val_transformed = [kn.data_raw[0]["id"]]
+        else:
+            val_transformed = kn.data_raw[0]["id"]
+
         LOOKUP_CACHE[f"{lookup_field_dest}{CACHE_DELIMTTER}{val}"] = val_transformed
         return val_transformed
     else:
@@ -70,6 +78,9 @@ def handle_html(html):
 
     E.g., email address
     """
+    if not html:
+        return None
+
     soup = BeautifulSoup(html, "html5lib")
     return soup.get_text()
 
