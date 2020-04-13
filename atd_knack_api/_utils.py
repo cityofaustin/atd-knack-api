10bbe8a6278a6228ee62d163636f22831d90097a
johnclary <john.clary@austintexas.gov>
2020-04-13 11:03:55 -0500

    flex note url setter

--- atd_knack_api/_utils.py
+++ atd_knack_api/_utils.py
@@ -1,6 +1,7 @@
 import knackpy
 
-def knackpy_wrapper(cfg, auth, filters=None):
+
+def knackpy_wrapper(cfg, auth, filters=None, raw_connections=False):
     """
     Fetch records which need to be processed from a pre-filtered
     Knack view which does not require authentication.
@@ -9,11 +10,13 @@ def knackpy_wrapper(cfg, auth, filters=None):
         scene=cfg.get("scene"),
         view=cfg.get("view"),
         obj=cfg.get("obj"),
+        ref_obj=cfg.get("ref_obj"),
         app_id=auth["app_id"],
         api_key=auth["api_key"],
         page_limit=100,
         rows_per_page=1000,
         filters=filters,
+        raw_connections=raw_connections,
     )
 
 
